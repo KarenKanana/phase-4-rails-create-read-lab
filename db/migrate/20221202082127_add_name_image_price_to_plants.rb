ff6b55795548c1c48cc5df08060bf3817398ad5b
Karen Kanana <kananakaren21@gmail.com>
2022-12-02 11:45:16 +0300

    Complete assignmnent

---
+++ db/migrate/20221202082127_add_name_image_price_to_plants.rb
@@ -0,0 +1,7 @@
+class AddNameImagePriceToPlants < ActiveRecord::Migration[6.1]
+  def change
+    add_column :plants, :name, :string
+    add_column :plants, :image, :string
+    add_column :plants, :price, :decimal
+  end
+end
